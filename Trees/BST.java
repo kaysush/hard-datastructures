98db9264a54bb25f00deaad29ce3537e6f888cac
kaysush <0120sushil@gmail.com>
2013-07-18 21:56:04 +0530

    contains() implemented

--- Trees/BST.java
+++ Trees/BST.java
@@ -41,6 +41,22 @@ public class BST <T extends Comparable> {
 
 	}
 
+	public boolean contains(T data){
+		return contains(data, root);
+	}
+
+	private boolean contains(T data , Node node){
+		if(node == null) return false;
+
+		int result = data.compareTo(node.data);
+		if(result < 0) 
+			return contains(data,node.left);
+		else if(result > 0)
+			return contains(data, node.right);
+		else 
+			return true;
+	}
+
 	public void traverse(){
 		StringBuilder buffer = new StringBuilder();
 		inorder_traverse(root , buffer);
@@ -71,5 +87,10 @@ public class BST <T extends Comparable> {
 		tree.insert(1);
 		tree.insert(3);
 		System.out.println(tree);
+		if(tree.contains(4)){
+			System.out.println("Tree contains the given data");
+		} else {
+			System.out.println("Tree doesn't contains the given data");
+		}
 	}
 }
